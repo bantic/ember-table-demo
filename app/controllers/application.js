d4915f18f980d529322ce332b6610eb222698a17
Cory Forsyth <cory.forsyth@addepar.com>
2019-07-24 18:06:45 -0400

    basic-table: Add ember-table component to application.hbs with 3 rows of sample data

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,19 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
-export default Controller.extend({});
+export default Controller.extend({
+  columns: computed(function() {
+    return [
+      { name: 'Ticker Symbol', valuePath: 'symbol', textAlign: 'center' },
+      { name: 'Price (USD)', valuePath: 'price', textAlign: 'right' }
+    ];
+  }),
+
+  rows: computed(function() {
+    return [
+      { symbol: 'AAPL', price: 105.5 },
+      { symbol: 'MSFT', price: 85.5 },
+      { symbol: 'SNAP', price: 15.5 }
+    ];
+  })
+});
