bb82f45704877f986bed867edc0dc00da335a2c2
Cory Forsyth <cory.forsyth@addepar.com>
2019-07-24 22:48:58 -0400

    row-colors: use color-ramp for row colors

--- app/helpers/row-class.js
+++ app/helpers/row-class.js
@@ -1,12 +1,9 @@
 import { helper } from '@ember/component/helper';
+import colorRamp from '../utils/color-ramp';
 
 export function rowClass([rowValue] /*, hash*/) {
   let pctChange = parseFloat(rowValue.pctChange);
-  if (pctChange > 0) {
-    return 'bg-green-200';
-  } else if (pctChange < 0) {
-    return 'bg-red-200';
-  }
+  return colorRamp(pctChange);
 }
 
 export default helper(rowClass);
